635c4ddd6db26b8000b5ea14cb766aeb3bd0f80c
rohan.luktuke <rohan.luktuke@lab49.com>
2014-08-14 17:39:54 -0400

    initial checkin

---
+++ site-demo/src/main/java/com/lab49/siteDemo/model/Zoo.java
@@ -0,0 +1,44 @@
+package com.lab49.siteDemo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Where we keep {@link Animal}s when in captivity.
+ * 
+ * @author ross.romano
+ */
+public class Zoo {
+
+	private List<Animal> animals;
+
+	/**
+	 * Default Constructor.
+	 */
+	public Zoo() {
+		animals = new ArrayList<Animal>();
+	}
+
+	/**
+	 * Adds an {@link Animal} to the {@link Zoo}.
+	 * 
+	 * @param animal
+	 *            {@link Animal} to add.
+	 */
+	public void addAnimal(Animal animal) {
+		animals.add(animal);
+	}
+
+	/**
+	 * Retrieves all the {@link Animal}s in the {@link Zoo}.
+	 * <p/>
+	 * <img src="doc-files/zoo_getAnimals.png" type="image/png" width="100"
+	 * height="200">
+	 * 
+	 * @return The {@link Animal}s.
+	 */
+	public List<Animal> getAnimals() {
+		return this.animals;
+	}
+
+}
